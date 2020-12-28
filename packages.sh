997f70d94616a419b8333f6532f02ee0a83f5b8c
hzjflying1234 <60028407+hzjflying1234@users.noreply.github.com>
2020-12-28 16:16:40 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 svn co https://github.com/pexcn/openwrt-udpspeeder/trunk/ ./udpspeeder
-svn co https://github.com/kenzok8/openwrt-packages
-svn co https://github.com/kenzok8/small
+svn co https://github.com/kenzok8/openwrt-packages/trunk/
+svn co https://github.com/kenzok8/small/trunk/
 rm -rf ./*/.git
 rm -rf ./*/.svn 
 rm -f .gitattributes .gitignore
